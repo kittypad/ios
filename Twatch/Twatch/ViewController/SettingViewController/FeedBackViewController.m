c1f55aac1e11d6227f72cf7adb72d1ddea6447ee
wangjizeng413 <wangjizeng413@126.com>
2013-10-31 21:20:40 +0800

    修改feedback

--- Twatch/Twatch/ViewController/SettingViewController/FeedBackViewController.m
+++ Twatch/Twatch/ViewController/SettingViewController/FeedBackViewController.m
@@ -28,7 +28,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    UIPlaceHolderTextView *feedBackTextField = [[UIPlaceHolderTextView alloc] initWithFrame:CGRectMake(20, 80, 280, 200)];
+    UIPlaceHolderTextView *feedBackTextField = [[UIPlaceHolderTextView alloc] initWithFrame:CGRectMake(20, 80, 280, 160)];
     feedBackTextField.showsVerticalScrollIndicator = YES;
     feedBackTextField.showsHorizontalScrollIndicator = YES;
     [feedBackTextField setPlaceholder:@"亲，求意见^_^"];
@@ -39,14 +39,39 @@
     feedBackTextField.layer.borderColor = color.CGColor;
     [self.view addSubview:feedBackTextField];
     
-    UIButton *sendBtn = [[UIButton alloc] initWithFrame:CGRectMake(240, 285, 60, 30)];
+    UIButton *sendBtn = [[UIButton alloc] initWithFrame:CGRectMake(240, 245, 60, 30)];
     
     [sendBtn setBackgroundColor:[UIColor blueColor]];
     [sendBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [sendBtn setTitle:@"发送" forState:UIControlStateNormal];
     [self.view addSubview:sendBtn];
+    
+    NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
+    [defaultCenter addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    [defaultCenter addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+}
+-(void)keyboardWillShow:(NSNotification*)notification{
+    //调整UI位置
+    NSTimeInterval animationDuration = 0.25f;
+    [UIView beginAnimations:@"move" context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    if ([[UIScreen mainScreen] bounds].size.height < 568)
+    {
+        self.view.frame = CGRectMake(0, -45, 320, self.view.frame.size.height);
+    }
+
+    [UIView commitAnimations];
 }
 
+- (void)keyboardWillHide:(NSNotification *)aNotification
+{
+    NSTimeInterval animationDuration = 0.25f;
+    [UIView beginAnimations:@"move" context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    self.view.center=CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+    
+    [UIView commitAnimations];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
