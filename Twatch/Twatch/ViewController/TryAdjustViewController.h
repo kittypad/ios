78bf70132e4c2bf1f1fe914ad14b06813c841c3d
gongtao <gongtao@jike.com>
2013-11-01 14:11:30 +0800

    添加滤镜界面

---
+++ Twatch/Twatch/ViewController/TryAdjustViewController.h
@@ -0,0 +1,13 @@
+//
+//  TryAdjustViewController.h
+//  Twatch
+//
+//  Created by 龚涛 on 11/1/13.
+//  Copyright (c) 2013 龚涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TryAdjustViewController : UIViewController
+
+@end
---
+++ Twatch/Twatch/ViewController/TryAdjustViewController.m
@@ -0,0 +1,38 @@
+//
+//  TryAdjustViewController.m
+//  Twatch
+//
+//  Created by 龚涛 on 11/1/13.
+//  Copyright (c) 2013 龚涛. All rights reserved.
+//
+
+#import "TryAdjustViewController.h"
+
+@interface TryAdjustViewController ()
+
+@end
+
+@implementation TryAdjustViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
