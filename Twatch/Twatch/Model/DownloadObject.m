4e892ef4d1fce07139d9ad52faacbbb719480c37
Gong Tao <gongtao@jike.com>
2013-12-04 20:51:27 +0800

    删除数据库

--- Twatch/Twatch/Model/DownloadObject.m
+++ Twatch/Twatch/Model/DownloadObject.m
@@ -8,6 +8,21 @@
 
 #import "DownloadObject.h"
 
+#define kApkUrl                 @"apkUrl"
+#define kIconUrl                @"iconUrl"
+#define kIntro                  @"intro"
+#define kName                   @"name"
+#define kPkg                    @"pkg"
+#define kSize                   @"size"
+#define kType                   @"type"
+#define kVer                    @"ver"
+#define kState                  @"state"
+
+#define kNotDownload            0
+#define kDownloading            1
+#define kNotInstall             2
+#define kInstalled              3
+
 @implementation DownloadObject
 
 @synthesize apkUrl;
@@ -19,5 +34,34 @@
 @synthesize type;
 @synthesize ver;
 @synthesize state;
+
+- (id)initWithCoder:(NSCoder *)coder
+{
+    if (self = [super init]) {
+        self.apkUrl = [coder decodeObjectForKey:kApkUrl];
+        self.iconUrl = [coder decodeObjectForKey:kIconUrl];
+        self.intro = [coder decodeObjectForKey:kIntro];
+        self.name = [coder decodeObjectForKey:kName];
+        self.pkg = [coder decodeObjectForKey:kPkg];
+        self.size = [coder decodeObjectForKey:kSize];
+        self.type = [coder decodeObjectForKey:kType];
+        self.ver = [coder decodeObjectForKey:kVer];
+        self.state = [coder decodeObjectForKey:kState];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder
+{
+    [coder encodeObject:self.apkUrl forKey:kApkUrl];
+    [coder encodeObject:self.iconUrl forKey:kIconUrl];
+    [coder encodeObject:self.intro forKey:kIntro];
+    [coder encodeObject:self.name forKey:kName];
+    [coder encodeObject:self.pkg forKey:kPkg];
+    [coder encodeObject:self.size forKey:kSize];
+    [coder encodeObject:self.type forKey:kType];
+    [coder encodeObject:self.ver forKey:kVer];
+    [coder encodeObject:self.state forKey:kState];
+}
 
 @end
--- Twatch/Twatch/Supporting Files/AppDelegate.h
+++ Twatch/Twatch/Supporting Files/AppDelegate.h
@@ -13,8 +13,4 @@
 @property (strong, nonatomic) UIWindow *window;
 @property (nonatomic)BOOL  haveNewVersion;
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
 @end
